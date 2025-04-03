61007940970f374cfe0ddceb5767420d0fa4f410
bryanxtong <bryanxtong@gmail.com>
2025-04-03 11:24:19 +0800

    after batch mode enabled, consumer side always have a Message<List<JSONObject> and not Message<List<SimpleMsg> for spring cloud stream rocketmq

--- SpringCloudStream/RocketMQ/rocketmq-message-push-example/src/main/java/com/example/rocketmq/push/BatchConsumer.java
+++ SpringCloudStream/RocketMQ/rocketmq-message-push-example/src/main/java/com/example/rocketmq/push/BatchConsumer.java
@@ -1,5 +1,6 @@
 package com.example.rocketmq.push;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,24 @@ import java.util.function.Consumer;
 
 @Component
 public class BatchConsumer {
+/*
     @Bean
     public Consumer<Message<List<SimpleMsg>>> inputBatch() {
         return message -> {
             List<SimpleMsg> payloads = message.getPayload();
             System.out.println("Payloads received: " + payloads);
+        };
+    }
+*/
+
+    @Bean
+    public Consumer<Message<List<JSONObject>>> inputBatch() {
+        return message -> {
+            List<JSONObject> payloads = message.getPayload();
+            payloads.forEach(payload -> {
+                SimpleMsg simpleMsg = payload.toJavaObject(SimpleMsg.class);
+                System.out.println("Payloads received: " + simpleMsg);
+            });
         };
     }
 }
