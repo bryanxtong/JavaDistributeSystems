1563f8a2bb328f1462abccefccba511ba12610ed
bryanxtong <bryanxtong@gmail.com>
2025-04-09 09:17:42 +0800

    Kafka producer sends messages and process in batch and then consumer consume them

--- SpringCloudStream/Kafka/kafka-message-batch-poll-consumer/src/main/java/com/example/kafka/consumer/KafkaMessagePollBatchConsumerApplication.java
+++ SpringCloudStream/Kafka/kafka-message-batch-poll-consumer/src/main/java/com/example/kafka/consumer/KafkaMessagePollBatchConsumerApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Bean;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * It supports to consume the messages from the topic in batch for kafka
@@ -24,10 +26,21 @@ public class KafkaMessagePollBatchConsumerApplication {
     @Bean
     public Supplier<SimpleMsg> supplier() {
        return ()-> {
-           SimpleMsg simpleMsg = new SimpleMsg("Test message sent: " + counter.incrementAndGet());
+           SimpleMsg simpleMsg = new SimpleMsg("Test message sent: ");
            System.out.println("sending messages ===>" + simpleMsg);
            return simpleMsg;
        };
+    }
+
+    @Bean
+    Function<List<SimpleMsg>, List<SimpleMsg>> process() {
+       return simpleMsgs -> {
+           return simpleMsgs
+                   .stream()
+                   .map(simpleMsg -> new SimpleMsg(simpleMsg.getMsg() + counter.incrementAndGet()))
+                   .collect(Collectors.toList());
+       };
+
     }
 
     @Bean
