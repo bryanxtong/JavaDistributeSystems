6ac4eb04eb328bba7f5239cf098e2b0bf76fef26
bryanxtong <bryanxtong@gmail.com>
2025-04-16 13:01:20 +0800

    add spring data redis rate limit for spring cloud gateway(as a filter)

---
+++ oauth2-gateway/spring-cloud-gateway-oauth2/src/main/java/com/example/gateway/oauth2/RateLimitConfig.java
@@ -0,0 +1,14 @@
+package com.example.gateway.oauth2;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfig {
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+}
