5fd54ddd4b0f50dbb4624ba90160441dd73a1c7a
bryanxtong <bryanxtong@gmail.com>
2025-04-17 09:15:20 +0800

    fallback handler produces error json response

--- oauth2-gateway/spring-cloud-gateway-oauth2/src/main/java/com/example/gateway/oauth2/CircuitBreakerFallBackEndpoints.java
+++ oauth2-gateway/spring-cloud-gateway-oauth2/src/main/java/com/example/gateway/oauth2/CircuitBreakerFallBackEndpoints.java
@@ -3,8 +3,10 @@ package com.example.gateway.oauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.*;
-import reactor.core.publisher.Mono;
+import java.util.Map;
 
 @Configuration
 public class CircuitBreakerFallBackEndpoints {
@@ -12,16 +14,17 @@ public class CircuitBreakerFallBackEndpoints {
     public RouterFunction<ServerResponse> routerFunction() {
         return RouterFunctions
                 .route()
-                .GET("/user-fallback", userFallBackGetEndpoint())
-                .POST("/user-fallback", userFallBackPostEndpoint())
+                .GET("/user-fallback", userFallBackEndpoint())
+                .POST("/user-fallback", userFallBackEndpoint())
                 .build();
     }
 
-    public HandlerFunction<ServerResponse> userFallBackGetEndpoint() {
-        return request -> ServerResponse.ok().body(Mono.just("user fall back due to circuitbreaker"), String.class);
-    }
-
-    public HandlerFunction<ServerResponse> userFallBackPostEndpoint() {
-        return request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+    public HandlerFunction<ServerResponse> userFallBackEndpoint() {
+        return request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(Map.of("status", "SERVICE UNAVAILABLE",
+                        "message", "User service is currently unavailable. Please try again later.",
+                        "code", "503"))
+                );
     }
 }
