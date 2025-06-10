dedaa4350aa57e10655915a14382142eb2aca819
bryanxtong <bryanxtong@gmail.com>
2025-06-10 08:31:08 +0800

    remove all the spring cloud and spring cloud alibaba dependencies and use dubbo native dependency support libraries.

--- dubbo/triple/DubboConsumer/src/main/java/com/example/dubbo/SimpleController.java
+++ dubbo/triple/DubboConsumer/src/main/java/com/example/dubbo/SimpleController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class SimpleController {
 
-    @DubboReference
+    @DubboReference(loadbalance = "roundrobin")
     private SimpleService simpleService;
 
     /**
--- dubbo/triple/DubboProvider/src/main/java/com/example/dubbo/DubboProviderApplication.java
+++ dubbo/triple/DubboProvider/src/main/java/com/example/dubbo/DubboProviderApplication.java
@@ -3,10 +3,8 @@ package com.example.dubbo;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableDubbo
 public class DubboProviderApplication {
 
