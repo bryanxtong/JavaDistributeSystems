802d49817fd7c9191ff45c24a438a957d835cc78
bryanxtong <bryanxtong@gmail.com>
2025-06-10 15:09:38 +0800

    add sentinel support and config for sentinel dashboard in sentinel.properties. degrade rule and flow control rule can apply on the dashboard.
    FlowException and DegradeException can be processed by dubbo client side flow control and degrade control, but if the server side has both of them, we just give an internal error.

--- dubbo/triple/DubboConsumer/src/main/java/com/example/dubbo/DubboConsumerApplication.java
+++ dubbo/triple/DubboConsumer/src/main/java/com/example/dubbo/DubboConsumerApplication.java
@@ -1,14 +1,24 @@
 package com.example.dubbo;
 
+import com.alibaba.csp.sentinel.adapter.dubbo3.config.DubboAdapterGlobalConfig;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 @EnableDubbo
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DubboConsumerApplication.class, args);
+    }
+
+    @Bean
+    public DubboConsumerFallBack sentinelConsumerFallback(){
+        DubboConsumerFallBack dubboConsumerFallBack = new DubboConsumerFallBack();
+        DubboAdapterGlobalConfig.setConsumerFallback(dubboConsumerFallBack);
+        return dubboConsumerFallBack;
     }
 
 }
---
+++ dubbo/triple/DubboConsumer/src/main/java/com/example/dubbo/DubboConsumerFallBack.java
@@ -0,0 +1,15 @@
+package com.example.dubbo;
+
+import com.alibaba.csp.sentinel.adapter.dubbo3.fallback.DubboFallback;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.apache.dubbo.rpc.AsyncRpcResult;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+
+public class DubboConsumerFallBack implements DubboFallback {
+    @Override
+    public Result handle(Invoker<?> invoker, Invocation invocation, BlockException ex) {
+        return AsyncRpcResult.newDefaultAsyncResult(ex, invocation);
+    }
+}
